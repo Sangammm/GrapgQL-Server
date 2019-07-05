11b7d36287add8f81fe7611fd7e3f66f2ef9d597
Sangam <sangamrajpara@gmail.com>
2019-07-05 09:50:16 +0530

    added some mutations

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+
+const resolvers = {
+  Query: {
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
+  },
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    }
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
+server.start(() => console.log("server is running on -->"));
